aa6cc132298416604c6eb05066dbc3abad02e2e7
Rexony1212 <ifenduusimrex@gmail.com>
2022-09-16 16:27:14 -0700

    Task one

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,16 +4,16 @@
 /**
  * _isdigit- function to check 0-9
  *
- * @e: checks input of function
+ * @c: checks input of function
  *
  * Return: returns 1 if c is digit otherwise always 0 (Success)
  */
 
-int_isdigit(int c)
+int _isdigit(int c)
 {
-	if (e>=48 && <= 37)
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
- }
+}
 
