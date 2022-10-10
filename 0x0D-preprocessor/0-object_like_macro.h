f7da6010f5cf6a9bb1637e28b7722e94d922048c
Rexony1212 <ifenduusimrex@gmail.com>
2022-10-10 14:00:24 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef 0-OBJECT_LIKE_MACRO_H
+#define 0-OBJECT_LIKE_MACRO_H
+
+/*
+ * File: 0-object_like_macro.h
+ * Auth: Rex Ifendu
+ * Desc: Header file that defines a macro named
+ * 	 SIZE as an abbreviation for the token 1024.
+ */
+
+#define SIZE 1024
+
+#endif
